673b70000bd3b1f76087a1a49ea813f06ddf89f9
skymanaditya1 <skymanaditya1@gmail.com>
2021-05-21 11:57:30 +0530

    Added models, resolved favicon.ico issue
    Added all the model files and tested it by adding content in the database from the admin portal.

--- questions/models.py
+++ questions/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from quizzes.models import Quiz
+from lipquiz.models import VideoQuiz
 
 # Create your models here.
 class Question(models.Model):
@@ -22,3 +23,25 @@ class Answer(models.Model):
 
     def __str__(self):
         return f"Question : {self.question.text}, Answer : {self.text}, Correct : {self.correct}"
+
+
+class SingleWordQuestion(models.Model):
+    text = models.CharField(max_length=200)
+    video_path = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    quiz = models.ForeignKey(VideoQuiz, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.text)
+
+    def get_answers(self):
+        return singlewordanswer_set.all()
+
+class SingleWordAnswer(models.Model):
+    text = models.CharField(max_length=200)
+    correct = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(SingleWordQuestion, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Question: {self.question}, Answer: {self.text}, Correct: {self.correct}"
