cae3bd1f3f7b0159fe8ef29149c8ab866f98b433
skymanaditya1 <skymanaditya1@gmail.com>
2021-05-22 05:24:43 +0530

    Modal dialog box implementation V1
    Modal working for open quiz. Added modal dialog box implementation for opening a quiz when a quiz is selected.

--- lipquiz/static/lipquiz/main.js
+++ lipquiz/static/lipquiz/main.js
@@ -2,12 +2,32 @@ console.log("Inside the main.js file")
 
 // register the onclick listeners for all the buttons 
 const quizButtons = [...document.getElementsByClassName("start-quiz-button")]
+const modalBody = document.getElementById("model-body-confirm")
+const startButton = document.getElementById("start-button")
 url = window.location.href 
 
 quizButtons.forEach(quizButton => quizButton.addEventListener('click', () => {
     const name = quizButton.getAttribute("data-quiz")
     const pk = quizButton.getAttribute("data-pk")
+    const questions = quizButton.getAttribute("data-questions")
+    const description = quizButton.getAttribute("data-description")
+    const difficulty = quizButton.getAttribute("data-difficulty")
+    const quiz_type = quizButton.getAttribute("data-quiz-type")
     console.log(name)
     console.log(pk)
-    window.location.href = url + pk
+
+    modalBody.innerHTML = `
+    <div class="h5 mb-3">Are you sure you want to begin "<b>${name}</b>"?</div>
+    <div class="text-muted">
+        <ul>
+            <li>Description: <b>${description}</b></li>
+            <li>Questions : <b>${questions}</b></li>        
+            <li>Difficulty: <b>${difficulty} mins</b></li>
+            <li>Quiz Type: <b>${quiz_type}%</b></li>
+        <ul>
+    </div>
+    `
+    startButton.addEventListener('click', () => {
+        window.location.href = url + pk
+    })
 }))
