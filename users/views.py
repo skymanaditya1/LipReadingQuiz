20d790f2302b4b2706f16f367e0c2ff6efcc6969
skymanaditya1 <skymanaditya1@gmail.com>
2021-05-25 02:16:36 +0530

    Added custom fields to registration page
    1. Added a custom fields to the user registration page as part of UserProfiles

--- users/views.py
+++ users/views.py
@@ -1,40 +1,42 @@
 from django.shortcuts import render, redirect
 #from users.forms import CustomUserCreationForm
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse
-from users.forms import SignUpForm
+from users.forms import SignUpForm, UserProfileForm
 
 # Create your views here.
 def dashboard(request):
-    return render(request, "users/dashboard.html")
-
-# User registration view 
-'''def register(request):
-    if request.method == "GET":
-        return render(
-            request, 
-            "users/register.html", 
-            {"form": CustomUserCreationForm} # this is the custom user creation form which is rendered by the browser
-        )
-    elif request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save() 
-            login(request, user)
-            return redirect(reverse("dashboard"))'''
+    if request.user.is_authenticated:
+        username = request.user.username
+    else:
+        username = "Not logged in"
+    
+    context = {'username' : username}
+    return render(request, 'users/dashboard.html', context)
 
 def register(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
-        if form.is_valid():
-            # save the form 
-            user = form.save() 
+        profile_form = UserProfileForm(request.POST)
+
+        if form.is_valid() and profile_form.is_valid():
+            user = form.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            profile.save()
+
             username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password')
-            # authenticate the user with his credentials 
-            #user = authenticate(username=username, password=raw_password)
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
             login(request, user)
+
             return redirect(reverse("dashboard"))
     else:
-        return render(request, 'users/register.html', {'form':SignUpForm})
+        form = SignUpForm()
+        profile_form = UserProfileForm()
+
+    # This will also handle the case where the form was not valid 
+    context = {'form' : form, 'profile_form' : profile_form}
+    return render(request, 'users/register.html', context)
